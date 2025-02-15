5bcf8d886f4dddb389e1d036327b37f1b5040696
Aatmik-06 <adi120kush@gmail.com>
2025-02-15 16:54:26 +0530

    inserting node at front of linked list

---
+++ Linked-List/Insertion_LinkedList_End/src/Node.java
@@ -0,0 +1,15 @@
+public class Node {
+    public int data ;
+    public Node next;
+
+
+    
+    public void insertNewNodeAtEnd(Node head , int data) {
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
